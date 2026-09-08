7d197508d0a743c15fe046530e1b4a4b3838f25e
ShivaniGujjar <100477281+ShivaniGujjar@users.noreply.github.com>
2026-09-08 14:26:38 +0530

    Time: 17 ms (16.99%) | Memory: 46.5 MB (25.20%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        
+        HashMap<Character, Integer> freq = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            
+            if (!freq.containsKey(c) || freq.get(c) == 0) {
+                return false;
+            }
+            
+            freq.put(c, freq.get(c) - 1);
+        }
+
+        
+        return true;
+    }
+}
